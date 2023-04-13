0e06248ad30e15175386f73581093cc644b03650
germanebarbosa <119632836+germanebarbosa@users.noreply.github.com>
2023-04-13 02:39:22 +0000

    Create a Random Domain

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//Code
+
+let pronoun = ["the", "our", "is", "just"];
+let adj = ["great", "big", "crazy", "unvelibable", "unique"];
+let noun = ["jogger", "racoon", "dog", "website", "love", "kittycats"];
+
+function getRandomDomain() {
+  let domain = `${getRandomElementArray(pronoun)}${getRandomElementArray(
+    adj
+  )}${getRandomElementArray(noun)}.com`;
+  return domain;
+}
+
+function getRandomElementArray(list) {
+  return list[Math.floor(Math.random() * list.length)];
+}
+
+let i = 0;
+do {
+  i++;
+  console.log(getRandomDomain());
+} while (i < 10);
+
+// for (let i = 0; i <= 10; i++) {
+//   console.log(getRandomDomain());
+// }
